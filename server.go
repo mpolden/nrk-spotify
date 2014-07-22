dffa586208b7db8c17dd6634ac2dac53faeb4b87
Martin Polden <martin.polden@gmail.com>
2014-07-22 17:26:21 +0200

    Use LRU cache for synced tracks

--- server.go
+++ server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/golang/groupcache/lru"
 	"github.com/mitchellh/colorstring"
 	"log"
 	"time"
@@ -11,26 +12,54 @@ type SyncServer struct {
 	Radio    *Radio
 	Interval time.Duration
 	playlist *Playlist
+	cache    *lru.Cache
 }
 
 func logColorf(format string, v ...interface{}) {
 	log.Printf(colorstring.Color(format), v...)
 }
 
+func (sync *SyncServer) isCached(track *Track) bool {
+	_, exists := sync.cache.Get(track.Id)
+	return exists
+}
+
+func (sync *SyncServer) addTrack(track *Track) {
+	if !sync.isCached(track) {
+		sync.cache.Add(track.Id, track)
+	}
+}
+
+func (sync *SyncServer) initPlaylist() error {
+	playlist, err := sync.Spotify.GetOrCreatePlaylist(sync.Radio.Name)
+	if err != nil {
+		return err
+	}
+	sync.playlist = playlist
+	return nil
+}
+
+func (sync *SyncServer) initCache() {
+	sync.cache = lru.New(100)
+	for _, t := range sync.playlist.Tracks.Items {
+		sync.addTrack(&t.Track)
+	}
+}
+
 func (sync *SyncServer) Serve() {
 	ticker := time.NewTicker(sync.Interval * time.Minute)
 
-	spotifyPlaylist, err := sync.Spotify.GetOrCreatePlaylist(
-		sync.Radio.Name)
-	if err != nil {
-		log.Fatalf("Failed to get or create playlist: %s", err)
-	}
-	sync.playlist = spotifyPlaylist
-
 	logColorf("Server started. Syncing every [bold]%d[reset] minute(s)",
 		sync.Interval)
+
+	if err := sync.initPlaylist(); err != nil {
+		log.Fatalf("Failed to get or create playlist: %s", err)
+	}
+	log.Printf("Playlist has %d entries", len(sync.playlist.Tracks.Items))
+	sync.initCache()
+
 	logColorf("[green]Tracks will be added to Spotify playlist: %s[reset]",
-		spotifyPlaylist.String())
+		sync.playlist.String())
 	f := func() {
 		logColorf("[light_magenta]Running sync[reset]")
 		if err := sync.run(); err != nil {
@@ -81,7 +110,7 @@ func (sync *SyncServer) run() error {
 				t.String(), err)
 			continue
 		}
-		if sync.playlist.Contains(*track) {
+		if sync.isCached(track) {
 			logColorf("[yellow]Already added: %s[reset]",
 				track.String())
 			continue
@@ -92,15 +121,9 @@ func (sync *SyncServer) run() error {
 				track.String(), err)
 			continue
 		}
-		// Refresh playlist
-		playlist, err := sync.Spotify.PlaylistById(sync.playlist.Id)
-		if err != nil {
-			logColorf("[red]Failed to refresh playlist: %s[reset]",
-				err)
-			continue
-		}
-		sync.playlist = playlist
-		logColorf("[green]Added track: %s[reset]", track.String())
+		sync.addTrack(track)
+		logColorf("[green]Added track: %s (cache size: %d)[reset]",
+			track.String(), sync.cache.Len())
 	}
 	return nil
 }
